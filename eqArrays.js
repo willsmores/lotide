9e79194e7a7f160527d160cf7d44930871993365
Steve Willsmore <scwillsmore@gmail.com>
2022-11-17 20:52:12 -0500

    Added code to handle zero length arrays and a check for different array lengths.

--- eqArrays.js
+++ eqArrays.js
@@ -8,13 +8,26 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(arr1, arr2) {
 
+  // Return false if the arrays are different lengths
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // Return true if both arrays are empty
+  if (arr1.length === 0 && arr2.length === 0) {
+    return true;
+  }
+
+  // Return false if either array is empty
+  if (arr1.length === 0 || arr2.length === 0) {
+    return false;
+  }
+  
   for (let i = 0; i < arr1.length; i++) {
-    for (let j = 0; j < arr2.length; j++) {
-      // Check the value at the same index of each array
-      if (arr1[i] !== arr2[i]) {
-        // If they don't match, the arrays are not equal
-        return false;
-      }
+    // Check the value at the same index of each array
+    if (arr1[i] !== arr2[i]) {
+      // If they don't match, the arrays are not equal
+      return false;
     }
   }
   // If the loop completes, the arrays are equal
@@ -22,6 +35,7 @@ const eqArrays = function(arr1, arr2) {
 };
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2]), true); // => should FAIL
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false); // => should FAIL
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
 assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true); // => should PASS
