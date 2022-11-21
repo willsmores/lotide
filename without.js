acba82cfe56c3361780d36acc2babc5a31bb1074
Steve Willsmore <scwillsmore@gmail.com>
2022-11-21 09:54:50 -0500

    Added updated eqArrays function.

--- without.js
+++ without.js
@@ -1,4 +1,20 @@
 const eqArrays = function(arr1, arr2) {
+
+  // Return false if the arrays are different lengths
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // Return true if both arrays are empty
+  if (arr1.length === 0 && arr2.length === 0) {
+    return true;
+  }
+
+  // Return false if either array is empty
+  if (arr1.length === 0 || arr2.length === 0) {
+    return false;
+  }
+  
   for (let i = 0; i < arr1.length; i++) {
     // Check the value at the same index of each array
     if (arr1[i] !== arr2[i]) {
